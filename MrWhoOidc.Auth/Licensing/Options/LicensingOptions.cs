61300dae1f53dd9b4e64d503fada2ed6baab8934
popicka70 <74075683+popicka70@users.noreply.github.com>
2025-10-26 10:01:27 +0100

    refactor: Change PublicKeyPem property to use expression-bodied member and remove unnecessary PostConfigure

--- MrWhoOidc.Auth/Licensing/Options/LicensingOptions.cs
+++ MrWhoOidc.Auth/Licensing/Options/LicensingOptions.cs
@@ -9,7 +9,7 @@ public sealed record LicensingOptions
 {
     public const string SectionName = "Licensing";
 
-    public string PublicKeyPem { get; init; } = EmbeddedLicensingKeys.PrimaryPublicKeyPem;
+    public string PublicKeyPem => EmbeddedLicensingKeys.PrimaryPublicKeyPem;
 
     public int CacheExpirationMinutes { get; init; } = 5;
 
@@ -30,7 +30,6 @@ public static class LicensingOptionsExtensions
         services
             .AddOptions<LicensingOptions>()
             .Bind(configuration.GetSection(LicensingOptions.SectionName))
-            .PostConfigure(options => options.PublicKeyPem = EmbeddedLicensingKeys.PrimaryPublicKeyPem)
             .ValidateOnStart();
 
         return services;
